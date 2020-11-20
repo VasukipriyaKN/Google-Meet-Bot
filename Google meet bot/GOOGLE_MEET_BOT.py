59a094b6613636a0b8ca9b4144b28cb4b582e9a8
Vasukipriya KN <67868950+VasukipriyaKN@users.noreply.github.com>
2020-11-20 11:12:38 +0530

    Update GOOGLE_MEET_BOT.py

--- Google meet bot/GOOGLE_MEET_BOT.py
+++ Google meet bot/GOOGLE_MEET_BOT.py
@@ -1,29 +1,31 @@
 from selenium import webdriver
 import pyautogui
 import time
-from selenium.webdriver.common.action_chains import ActionChains 
-from twilio.rest import Client 
+from selenium.webdriver.common.action_chains import ActionChains
+from twilio.rest import Client
 from playsound import playsound
 from screen_recorder_sdk import screen_recorder
+# USING THHIS YOU CAN RECORD SCREEN AND SAVE IT LOCALLY
 
 
 def msg():
-  
-    # Your Account Sid and Auth Token from twilio.com / console 
-    account_sid = '<ACCOUNT SID>'                           #FILL HERE
-    auth_token = '<TOKEN>'                                  #FILL HERE
-    
-    client = Client(account_sid, auth_token) 
-    
+
+    # Your Account Sid and Auth Token from twilio.com / console
+    account_sid = '<ACCOUNT SID>'                                                        # FILL ACCOUNT SID HERE
+    auth_token = '<TOKEN>'                                                           # FILL AUTHENTICATION TOKEN HERE
+
+    client = Client(account_sid, auth_token)
+
     ''' Change the value of 'from' with the number  
     received from Twilio and the value of 'to' 
     with the number in which you want to send message.'''
-    message = client.messages.create( 
-                                from_='<CONTACT NO.>',      #FILL HERE
-                                body ='<MESSAGE>',          #FILL HERE
-                                to ='<CONTACT NO.>'         #FILL HERE
-                            ) 
+    message = client.messages.create(
+        from_='<CONTACT NO.>',                                            # FILL SENDERS CONTACT NUMBER HERE
+        body='<MESSAGE>',                                              # FILL THE MESSAGE WHICH YOU WANT RECEIVE HERE
+        to='<CONTACT NO.>'                                                 # FILL RECEIVERS CONTACT NUMBER HERE
+    )
     print(message.sid)
+
 
 def automsg():
     msg()
@@ -31,9 +33,10 @@ def automsg():
     element = browser.find_element_by_class_name('HKarue')
     element.click()
     time.sleep(5)
-    pyautogui.write("<YOUR_NAME> Present")                      #ENTER YOUR NAME
+    pyautogui.write("<YOUR_NAME> Present")                                         # ENTER YOUR NAME
     pyautogui.press("enter")
-    playsound('<PATH ADDRESS OF RECORDED AUDIO>')               #PATH OF RECORDED AUDIO
+    playsound('<PATH ADDRESS OF RECORDED AUDIO>')                                     # PATH OF RECORDED AUDIO
+
 
 driver = webdriver.ChromeOptions()
 driver.add_argument("--incognito")
@@ -41,12 +44,12 @@ driver.add_argument("--incognito")
 caps = driver.to_capabilities()
 
 browser = webdriver.Chrome(desired_capabilities=caps)
-browser.get('https://accounts.google.com/signin/v2/identifier?ltmpl=meet&continue=https%3A%2F%2Fmeet.google.com%3Fhs%3D193&&o_ref=https%3A%2F%2Fwww.google.com%2F&_ga=2.27245821.19144059.1605760032-822222673.1605760032&flowName=GlifWebSignIn&flowEntry=ServiceLogin')
+browser.get('https: // accounts.google.com/signin/v2/identifier?ltmpl=meet & continue=https % 3A % 2F % 2Fmeet.google.com % 3Fhs % 3D193 & &o_ref=https % 3A % 2F % 2Fwww.google.com % 2F & _ga=2.27245821.19144059.1605760032-822222673.1605760032 & flowName=GlifWebSignIn & flowEntry=ServiceLogin')
 time.sleep(6)
-pyautogui.write("<EMAIL ID>")                                   #FILL HERE
+pyautogui.write("<EMAIL ID>")                                 # FILL YOUR MAIL ID HERE
 pyautogui.press("enter")
 time.sleep(2.5)
-pyautogui.write("<PASSWORD>")                                   #FILL HERE
+pyautogui.write("<PASSWORD>")                                # FILL YOUR PASSWORD HERE
 pyautogui.press("enter")
 time.sleep(7)
 pyautogui.press("enter")
@@ -54,6 +57,7 @@ time.sleep(10)
 pyautogui.press("esc")
 time.sleep(3)
 pyautogui.click(x=1310, y=600)
+                                                               # SOMETIMES THIS VALUE CAN CHANGE BASED ON SCREEN SIZE
 time.sleep(5)
 
 element = browser.find_element_by_class_name('n8i9t')
@@ -62,5 +66,6 @@ element.click()
 time.sleep(0.5)
 element = browser.find_element_by_class_name('aGJE1b')
 
-if (element.get_attribute('innerHTML')=="<YOUR NAME OR ROLL NUM>"):     #TYPE ON WHAT WORDS YOU WANT TO GET NOTIFIED
+# TYPE ON WHAT WORDS YOU WANT TO GET NOTIFIED
+if (element.get_attribute('innerHTML') == "<YOUR NAME OR ROLL NUM>"):
     automsg()
